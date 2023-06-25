aad32e86ce32da3c00178981b79455c97ef0a47c
Kyle Fortuin <kylef@bbd.co.za>
2023-06-25 19:50:33 +0200

    folder restructure

--- Resource Server/routes/db.routes.js
+++ Resource Server/routes/db.routes.js
@@ -3,6 +3,7 @@ const path = require("path");
 const router = express.Router();
 
 const { getUser, newUser, listHighScores, updateScore } = require('../middleware/dbMiddleware');
+<<<<<<< HEAD
 const { ensureAuthenticated } = require('../middleware/authMiddleware');
 
 
@@ -10,5 +11,12 @@ router.get('/highscores', ensureAuthenticated, listHighScores);
 router.get('/user', ensureAuthenticated, getUser);
 router.post('user/new', ensureAuthenticated, newUser);
 router.post('user/score', ensureAuthenticated, updateScore);
+=======
+
+router.get('/highscores', listHighScores);
+router.get('/user', getUser);
+router.post('user/new', newUser);
+router.post('user/score', updateScore);
+>>>>>>> 083d8b6 (folder restructure)
 
 module.exports = router;
