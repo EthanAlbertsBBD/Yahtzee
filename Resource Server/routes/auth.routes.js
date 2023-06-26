eb088dc6ff12f102e73a8d576e67bbfeee7a4ae9
Ethan Alberts <ethana@bbd.co.za>
2023-06-26 20:05:29 +0200

    Team changes

--- Resource Server/routes/auth.routes.js
+++ Resource Server/routes/auth.routes.js
@@ -1,50 +1,49 @@
-const express = require("express");
-const passport = require("passport");
+const express = require('express');
+const passport = require('passport');
 const router = express.Router();
-require("../controllers/passport");
+require('../controllers/passport');
 const loginMiddleware = require('../middleware/loginMiddleware');
 
-
 router.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["email", "profile"],
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile'],
   })
 );
 
 router.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    successRedirect: "/gameboard",
-    failureRedirect: "/auth/google/failure",
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/gameboard',
+    failureRedirect: '/auth/google/failure',
   })
 );
 
-router.get("/auth/google/failure", (req, res) => {
-  res.send("Something went wrong with Google authentication.");
+router.get('/auth/google/failure', (req, res) => {
+  res.send('Something went wrong with Google authentication.');
 });
 
 router.get(
-    "/auth/github",
-    passport.authenticate("github", {
-        scope: ["email", "profile"]
-    })
+  '/auth/github',
+  passport.authenticate('github', {
+    scope: ['email', 'profile'],
+  })
 );
 
 router.get(
-    "/auth/github/callback",
-    passport.authenticate("github", {
-        successRedirect: "/gameboard",
-        failureRedirect: "/auth/github/failure"
-    })
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    successRedirect: '/gameboard',
+    failureRedirect: '/auth/github/failure',
+  })
 );
 
-router.get("/auth/github/failure", (req, res) => {
-    res.send("Something went wrong with Github authentication.");
+router.get('/auth/github/failure', (req, res) => {
+  res.send('Something went wrong with Github authentication.');
 });
 
-router.post("/login", loginMiddleware, (req, res) => {
-    res.redirect('/gameboard');
+router.post('/login', loginMiddleware, (req, res) => {
+  res.redirect('/gameboard');
 });
 
 module.exports = router;
--- Resource Server/server.js
+++ Resource Server/server.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const favicon = require("serve-favicon");
-const fs = require("fs");
-const https = require("https");
-const session = require("express-session");
-const passport = require("passport");
-const indexRoutes = require("./routes/index.routes");
-const authRoutes = require("./routes/auth.routes");
+const express = require('express');
+const favicon = require('serve-favicon');
+const fs = require('fs');
+const https = require('https');
+const session = require('express-session');
+const passport = require('passport');
+const indexRoutes = require('./routes/index.routes');
+const authRoutes = require('./routes/auth.routes');
 const dbRoutes = require('./routes/db.routes');
-require("dotenv").config();
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 // App configuration
 const app = express();
@@ -19,13 +20,15 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(passport.session());
 
 // Static files
-app.use(favicon("public/assets/favicon.png"));
-app.use(express.static("public/views/login"));
-app.use(express.static("public/views/gameboard"));
-app.use(express.static("public/views/leaderboard"));
+app.use(favicon('public/assets/favicon.png'));
+app.use(express.static('public/views/login'));
+app.use(express.static('public/views/gameboard'));
+app.use(express.static('public/views/leaderboard'));
 
 // Routes
 app.use(indexRoutes);
@@ -34,10 +37,10 @@ app.use(dbRoutes);
 
 // Server
 const options = {
-  key: fs.readFileSync("key.pem"),
-  cert: fs.readFileSync("cert.pem"),
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
 };
 
 https.createServer(options, app).listen(8000, () => {
-  console.log("https://localhost:8000");
+  console.log('https://localhost:8000');
 });
