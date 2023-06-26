78c96c4b10e81cf1acaf03bd60ac51b531a19865
G994618 <devlone@sanlaminvestments.com>
2023-06-26 15:51:35 +0200

    Moved files

--- Resource Server/routes/db.routes.js
+++ Resource Server/routes/db.routes.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const router = express.Router();
 
-const { getUser, newUser, listHighScores, updateScore } = require('../middleware/dbMiddleware');
+const {
+  getUser,
+  newUser,
+  listHighScores,
+  updateScore,
+} = require('../middleware/dbMiddleware');
+const { ensureAuthenticated } = require('../middleware/authMiddleware');
 
-router.get('/highscores', listHighScores);
-router.get('/user', getUser);
-router.post('user/new', newUser);
-router.post('user/score', updateScore);
+router.get('/highscores', ensureAuthenticated, listHighScores);
+router.get('/user', ensureAuthenticated, getUser);
+router.post('user/new', ensureAuthenticated, newUser);
+router.post('user/score', ensureAuthenticated, updateScore);
 
 module.exports = router;
