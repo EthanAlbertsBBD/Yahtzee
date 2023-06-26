5647fc76f9800770d745c9ed0fda3c78e5a538bd
G994618 <devlone@sanlaminvestments.com>
2023-06-26 16:33:08 +0200

    Some auth stuff to verify user

---
+++ Resource Server/middleware/loginMiddleware.js
@@ -0,0 +1,43 @@
+const https = require('https');
+require('dotenv').config();
+
+const loginMiddleware = (req, res, next) => {
+    // const loginData = {
+    //     username: req.body.username,
+    //     password: req.body.password
+    // };
+
+    const options = {
+        method: 'POST',
+        hostname: process.env.AUTH_SERVER_HOST,
+        port: process.env.AUTH_SERVER_PORT,
+        path: '/login',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const request = https.request(options, response => {
+        let data = '';
+
+        response.on('data', chunk => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            const { accessToken, refreshToken } = JSON.parse(data);
+            res.cookie('access_token', accessToken, { httpOnly: true, secure: true });
+            return next();
+        });
+    });
+
+    request.on('error', error => {
+        console.error('Request failed:', error);
+        res.status(500).send('Login failed');
+    });
+
+    // request.write(JSON.stringify(loginData));
+    request.end();
+};
+
+module.exports = loginMiddleware;
