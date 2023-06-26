f4340f5513d96c4c9fba93b05f5bb41bf4c6c3a5
Ethan Alberts <ethana@bbd.co.za>
2023-06-26 20:05:29 +0200

    Team changes

--- Resource Server/middleware/authMiddleware.js
+++ Resource Server/middleware/authMiddleware.js
@@ -1,11 +1,12 @@
 async function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated() || req.cookie.accessToken) {
-        // If user is authenticated, allow them to proceed
-        return next();
-    } else {
-        // If user is not authenticated, redirect to login page or show an error
-        res.redirect('/'); // Modify the URL according to your application's routes
-    }
+  //TODO Implemnt cookie parser
+  if (req.isAuthenticated() || req.cookies.accessToken) {
+    // If user is authenticated, allow them to proceed
+    return next();
+  } else {
+    // If user is not authenticated, redirect to login page or show an error
+    res.redirect('/'); // Modify the URL according to your application's routes
+  }
 }
 
 module.exports = { ensureAuthenticated };
--- Resource Server/middleware/loginMiddleware.js
+++ Resource Server/middleware/loginMiddleware.js
@@ -2,42 +2,42 @@ const https = require('https');
 require('dotenv').config();
 
 const loginMiddleware = (req, res, next) => {
-    const loginData = {
-        username: req.body.username,
-        password: req.body.password
-    };
+  const loginData = {
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-    const options = {
-        method: 'POST',
-        hostname: process.env.AUTH_SERVER_HOST,
-        port: process.env.AUTH_SERVER_PORT,
-        path: '/login',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
+  const options = {
+    protocol: 'https:',
+    method: 'POST',
+    hostname: process.env.AUTH_SERVER_HOST,
+    path: '/login',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-    const request = https.request(options, response => {
-        let data = '';
+  const request = https.request(options, (response) => {
+    let data = '';
 
-        response.on('data', chunk => {
-            data += chunk;
-        });
-
-        response.on('end', () => {
-            const { accessToken, refreshToken } = JSON.parse(data);
-            res.cookie('accessToken', accessToken, { httpOnly: true, secure: true });
-            next();
-        });
+    response.on('data', (chunk) => {
+      data += chunk;
     });
 
-    request.on('error', error => {
-        console.error('Request failed:', error);
-        res.status(500).send('Login failed');
+    response.on('end', () => {
+      const { accessToken, refreshToken } = JSON.parse(data);
+      res.cookie('accessToken', accessToken, { httpOnly: true, secure: true });
+      next();
     });
+  });
 
-    request.write(JSON.stringify(loginData));
-    request.end();
+  request.on('error', (error) => {
+    console.error('Request failed:', error);
+    res.status(500).send('Login failed');
+  });
+
+  request.write(JSON.stringify(loginData));
+  request.end();
 };
 
 module.exports = loginMiddleware;
